46cb305ad9329315c140767c8d2b60fbe74209e0
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-12 23:12:55 -0300

    Separando as Views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  res.send("home no controller!")
-  //res.render("pages/home");
+  res.render("pages/page");
 };
 
 export const dogs = (req: Request, res: Response) => {
+  res.send("dogs")
   //res.render("pages/home");
 };
 
 export const cats = (req: Request, res: Response) => {
+  res.send("cats")
   //res.render("pages/home");
 };
 
 export const fishes = (req: Request, res: Response) => {
+  res.send("fishes")
   //res.render("pages/home");
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,7 @@ router.get("/", PageController.home);
 router.get("/dogs", PageController.dogs);
 router.get("/cats", PageController.cats);
 router.get("/fishes", PageController.fishes);
+
 router.get("/search", SearchController.search);
 
 export default router;
