ca311f5ab793737db369c7dec607739f473c6903
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-12 23:02:03 -0300

    Criando as rotas e controllers

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
+/** Dependences */
 import express from "express";
 import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
+
+/** Imports */
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -13,6 +17,10 @@ server.engine("mustache", mustache());
 
 server.use(express.static(path.join(__dirname, "../public")));
 
-// Rotas
+server.use(mainRoutes);
+
+server.use((req, res) => {
+    res.send('Página não encontrada!')
+})
 
 server.listen(process.env.PORT);
